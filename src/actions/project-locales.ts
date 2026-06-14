95b4cd16237a066f60cdadf3d4cd4d2bfb49eb9c
Leon-JavaScript <70959426+Leon-JavaScript@users.noreply.github.com>
2026-06-14 19:25:18 +0200

    fix: disabled locales can be added by users to projects

--- src/actions/project-locales.ts
+++ src/actions/project-locales.ts
@@ -4,6 +4,8 @@ import { canManageProjectFeature } from "@/actions/projects"
 import { ProjectPermission } from "@/lib/project-permissions"
 import { ProjectLocale } from "@prisma/client"
 import { addLocale, removeLocale } from "@/services/project-locales"
+import { LocalesService } from "@/services/locales"
+import { auth } from "@/lib/auth"
 
 export async function addProjectLocale({
   projectId,
@@ -12,10 +14,32 @@ export async function addProjectLocale({
   projectId: string
   localeId: string
 }): Promise<ProjectLocale> {
-  const { project } = await canManageProjectFeature({
+  const { user, project } = await canManageProjectFeature({
     projectId,
     permission: ProjectPermission.MANAGE_LOCALES,
   })
+
+  const locale = await LocalesService.getLocale(localeId)
+
+  if (!locale) {
+    throw new Error(`Locale with ID "${localeId}" does not exist.`)
+  }
+
+  if (!locale.enabled) {
+    const hasPermission = (await auth.api.userHasPermission({
+      body: {
+        // @ts-expect-error - user.role can be any string, but the API expects a defined set of strings.
+        role: user.role ?? "user",
+        permissions: {
+          locales: ["read:disabled"],
+        },
+      },
+    })).success
+
+    if (!hasPermission) {
+      throw new Error(`Locale with ID "${localeId}" does not exist.`)
+    }
+  }
 
   return await addLocale({
     project,
